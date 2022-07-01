b0761468edfeca4160a3bda29c2fa510e48ad276
MohammadMir <mohammadmir9891@gmail.com>
2022-07-01 12:59:08 +0430

    onion-routing almost implemented

--- client/client.go
+++ client/client.go
@@ -1,9 +1,262 @@
 package main
 
-import "log"
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	NOTIF_SERVER_GET_NOTIFS_URL    = "http://localhost:8080/notifs"
+	NODE_DIRECTORRY_GET_RELAYS_URL = "http://localhost:9090/nodes"
+
+	PAYLOAD_ENCRYPTED_TYPE = "ENC"
+	PAYLOAD_REQUEST_TYPE   = "REQ"
+
+	GET_METHOD  = "GET"
+	POST_METHOD = "POST"
+
+	NEW_vC_PATH      = "/new-vc"
+	FORWARD_API_PATH = "/forward"
+)
+
+// {
+// vc-id, payload-type,
+// payload { method, url, body }
+// }
+
+type final_payload_struct struct {
+	Method string `json:"method"`
+	URL    string `json:"url"`
+	Body   []byte `json:"body"`
+}
+
+type relay_payload_struct struct {
+	VC_ID        string `json:"vc_id"`
+	Payload_type string `json:"payload_type"`
+	Payload      []byte `json:"payload"`
+}
+
+type relay_node_struct struct {
+	Name           string `json:"name"`
+	Port           int    `json:"port"`
+	Directory_node string `json:"dir_node"`
+}
+
+type vc_nodes_struct struct {
+	entry_node  relay_node_struct
+	middle_node relay_node_struct
+	exit_node   relay_node_struct
+}
 
 func main() {
 	log.Println("Client started...")
+	nodes := getRelayNodes()
+	fmt.Println("Choose three relay nodes :")
+	for i := 0; i < len(nodes); i++ {
+		fmt.Printf("%d. %s \t %d\n", i+1, nodes[i].Name, nodes[i].Port)
+	}
+	selected_nodes := getSelectedRelays(nodes)
+	new_vc := vc_nodes_struct{
+		entry_node:  selected_nodes[0],
+		middle_node: selected_nodes[1],
+		exit_node:   selected_nodes[2],
+	}
+	vc_id := "123456"
+	vc_w_entry := create_vc_with_entry_node(vc_id, new_vc.entry_node)
+	if vc_w_entry == false {
 
+	}
+	vc_w_middle := extend_vc_with_middle_node(vc_id, new_vc.middle_node, new_vc.entry_node)
+	if vc_w_middle == false {
+
+	}
+	log.Println("extending with middle node was successful")
+	vc_w_exit := extend_vc_with_exit_node(vc_id, new_vc.exit_node, new_vc.middle_node, new_vc.entry_node)
+	if vc_w_exit == false {
+		
+	}
+	log.Println("extending vc with exit node was successful")
 	
+	// final_payload := final_payload_struct{
+	// 	Method: GET_METHOD,
+	// 	URL:    NOTIF_SERVER_GET_NOTIFS_URL,
+	// }
+	// json_payload, err := json.Marshal(final_payload)
+	// if err != nil {
+
+	// }
+	// relay_3_payload := relay_payload_struct{
+	// 	VC_ID:        "1000",
+	// 	Payload_type: PAYLOAD_REQUEST_TYPE,
+	// 	Payload:      json_payload,
+	// }
+	// json_relay_3, err := json.Marshal(relay_3_payload)
+	// if err != nil {
+
+	// }
+	// resp, err := http.Post("http://localhost:10000/it-is-working", "application/json", bytes.NewBuffer(json_relay_3))
+	// if err != nil {
+
+	// }
+	// defer resp.Body.Close()
+	// log.Println(resp.StatusCode)
+
+}
+
+func getRelayNodes() []relay_node_struct {
+	log.Println("Sending Get request to dir_node...")
+	resp, err := http.Get(NODE_DIRECTORRY_GET_RELAYS_URL)
+	if err != nil {
+
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+
+	}
+	nodes_json_list, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+
+	}
+	var nodes []relay_node_struct
+	json_err := json.Unmarshal(nodes_json_list, &nodes)
+	if json_err != nil {
+
+	}
+
+	return nodes
+}
+
+func getSelectedRelays(nodes []relay_node_struct) []relay_node_struct {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+
+	var n []relay_node_struct
+	for _, f := range strings.Fields(scanner.Text()) {
+		i, err := strconv.Atoi(f)
+		if err == nil {
+			n = append(n, nodes[i-1])
+		}
+	}
+
+	return n
+}
+
+func create_vc_with_entry_node(vc_id string, node relay_node_struct) bool {
+	args := new_vc_struct{
+		VC_id: vc_id,
+	}
+	json_body, err := json.Marshal(args)
+	if err != nil {
+
+	}
+	resp, err := http.Post("http://localhost:"+strconv.Itoa(node.Port)+NEW_vC_PATH, "application/json", bytes.NewBuffer(json_body))
+	if err != nil {
+		return false
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusCreated {
+		return false
+	}
+	return true
+}
+
+func extend_vc_with_middle_node(vc_id string, middle_node relay_node_struct, entry_node relay_node_struct) bool {
+	args := new_vc_struct{
+		VC_id: vc_id,
+		Incomming_port: entry_node.Port,
+	}
+	args_json, err := json.Marshal(args)
+	if err != nil {
+		
+	}
+	final_payload := final_payload_struct{
+		Method: POST_METHOD,
+		URL:    "http://localhost:" + strconv.Itoa(middle_node.Port) + NEW_vC_PATH,
+		Body:   args_json,
+	}
+	json_payload, err := json.Marshal(final_payload)
+	if err != nil {
+
+	}
+	node_1_payload := relay_payload_struct{
+		VC_ID:        vc_id,
+		Payload_type: PAYLOAD_REQUEST_TYPE,
+		Payload:      json_payload,
+	}
+	json_node_1, err := json.Marshal(node_1_payload)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	log.Println(string(json_node_1))
+	resp, err := http.Post("http://localhost:"+strconv.Itoa(entry_node.Port)+FORWARD_API_PATH, "application/json", bytes.NewBuffer(json_node_1))
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	defer resp.Body.Close()
+	log.Println(resp.StatusCode)
+	return true
+}
+
+type new_vc_struct struct {
+	VC_id          string `json:"vc_id"`
+	Incomming_port int    `json:"incomming_port"`
+}
+
+func extend_vc_with_exit_node(vc_id string, exit_node relay_node_struct, middle_node relay_node_struct, entry_node relay_node_struct) bool{
+	args := new_vc_struct{
+		VC_id: vc_id,
+		Incomming_port: middle_node.Port,
+	}
+	args_json, err := json.Marshal(args)
+	if err != nil {
+		
+	}
+	final_payload := final_payload_struct{
+		Method: POST_METHOD,
+		URL:    "http://localhost:" + strconv.Itoa(exit_node.Port) + NEW_vC_PATH,
+		Body:   args_json,
+	}
+	json_payload, err := json.Marshal(final_payload)
+	if err != nil {
+
+	}
+	node_2_payload := relay_payload_struct{
+		VC_ID:        vc_id,
+		Payload_type: PAYLOAD_REQUEST_TYPE,
+		Payload:      json_payload,
+	}
+	json_node_2, err := json.Marshal(node_2_payload)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	node_1_payload := relay_payload_struct{
+		VC_ID:        vc_id,
+		Payload_type: PAYLOAD_ENCRYPTED_TYPE,
+		Payload:      json_node_2,
+	}
+	json_node_1, err := json.Marshal(node_1_payload)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	log.Println(string(json_node_1))
+	resp, err := http.Post("http://localhost:"+strconv.Itoa(entry_node.Port)+FORWARD_API_PATH, "application/json", bytes.NewBuffer(json_node_1))
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	defer resp.Body.Close()
+	log.Println(resp.StatusCode)
+	return true
 }
--- node/node.go
+++ node/node.go
@@ -1,11 +1,36 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
 	"net/url"
 	"strconv"
+	"strings"
+)
+
+const (
+	DIR_URL              = "http://localhost"
+	ADD_NODE_API_PATH    = "/nodes/add"
+	REMOVE_NODE_API_PATH = "/nodes/remove"
+	NEW_VC_API_PATH      = "/new-vc"
+	FORWARD_API_PATH     = "/forward"
+
+	GET_METHOD  = "GET"
+	POST_METHOD = "POST"
+
+	PAYLOAD_ENCRYPTED_TYPE = "ENC"
+	PAYLOAD_REQUEST_TYPE   = "REQ"
+
+	VC_ID_FIELD          = "vc_id"
+	PAYLOAD_TYPE_FIELD   = "payload_type"
+	PAYLOAD_FIELD        = "payload"
+	PAYLOAD_METHOD_FIELD = "method"
+	PAYLOAD_URL_FIELD    = "url"
+	PAYLOAD_BODY_FIELD   = "body"
 )
 
 type node struct {
@@ -14,16 +39,19 @@ type node struct {
 	directory_name string
 }
 
-const (
-	DIR_URL = "http://localhost"
-	ADD_NODE_API_PATH = "/nodes/add"
-	REMOVE_NODE_API_PATH = "/nodes/remove"
-)
+type vc struct {
+	id        string
+	key       string
+	pre_node  node
+	next_node node
+}
+
+var circuits = make([]vc, 0)
 
 func main() {
 	node_info := node{
-		name:           "node 1",
-		port:           10000,
+		name:           "node 3",
+		port:           13000,
 		directory_name: "9090",
 	}
 
@@ -47,10 +75,10 @@ func main() {
 
 func notify_dir_im_alive(node_info node) bool {
 	log.Println("Notifying directory_node I'm alive...")
-	resp, err := http.PostForm(DIR_URL + ":" + node_info.directory_name + ADD_NODE_API_PATH,
+	resp, err := http.PostForm(DIR_URL+":"+node_info.directory_name+ADD_NODE_API_PATH,
 		url.Values{"name": {node_info.name}, "dir_node": {node_info.directory_name}, "port": {strconv.Itoa(node_info.port)}})
 	if err != nil {
-		log.Println("node " + node_info.name + " can't send post req to notify dir_node. err :" + err.Error() )
+		log.Println("node " + node_info.name + " can't send post req to notify dir_node. err :" + err.Error())
 		return false
 	}
 	defer resp.Body.Close()
@@ -59,7 +87,7 @@ func notify_dir_im_alive(node_info node) bool {
 		return true
 	}
 	// if resp.StatusCode == http.StatusInternalServerError {
-	// 	return false		
+	// 	return false
 	// }
 	// if resp.StatusCode == http.StatusNotAcceptable {
 	// 	return false
@@ -69,7 +97,7 @@ func notify_dir_im_alive(node_info node) bool {
 
 func notify_dir_im_dead(node_info node) {
 	log.Println("Notifying dir_node I'm dead...")
-	resp, err := http.PostForm(DIR_URL + ":" + node_info.directory_name + REMOVE_NODE_API_PATH,
+	resp, err := http.PostForm(DIR_URL+":"+node_info.directory_name+REMOVE_NODE_API_PATH,
 		url.Values{"name": {node_info.name}})
 	if err != nil {
 		log.Println("node " + node_info.name + "can't send remove req to dir_node. err : " + err.Error())
@@ -81,5 +109,154 @@ func notify_dir_im_dead(node_info node) {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL.Path)
+	if r.URL.Path == NEW_VC_API_PATH && r.Method == POST_METHOD {
+		create_new_vc(w, r)
+		return
+	}
+	if r.URL.Path == FORWARD_API_PATH && r.Method == POST_METHOD {
+		forward_msg(w, r)
+		return
+	}
 
+	w.WriteHeader(http.StatusNotImplemented)
+
+}
+
+func create_new_vc(w http.ResponseWriter, r *http.Request) {
+	// vc_id := r.FormValue(VC_ID_FIELD)
+	json_body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer r.Body.Close()
+	log.Println(string(json_body))
+	var request new_vc_struct
+	if err := json.Unmarshal(json_body, &request); err != nil {
+
+	}
+	
+	log.Println("creating new vc with vc_id : " + request.VC_id)
+
+	// pre_node_port, err := strconv.Atoi(r.FormValue("incomming_port"))
+	// if err != nil {
+
+	// }
+	newVC := vc{
+		id:       request.VC_id,
+		pre_node: node{port: request.incomming_port},
+	}
+	circuits = append(circuits, newVC)
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func forward_msg(w http.ResponseWriter, r *http.Request) {
+	// vc_id := r.FormValue(VC_ID_FIELD)
+	// payload_type := r.FormValue(PAYLOAD_TYPE_FIELD)
+
+	json_body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer r.Body.Close()
+	log.Println(string(json_body))
+	var request payload_struct
+	if err := json.Unmarshal(json_body, &request); err != nil {
+
+	}
+	log.Println("forwading msg with vc_id : " + request.VC_id + " and payload_type : " + request.Payload_type)
+	if request.Payload_type == PAYLOAD_REQUEST_TYPE {
+		var final_payload final_payload_struct
+		err := json.Unmarshal([]byte(request.Payload), &final_payload)
+		if err != nil {
+			log.Println("error occured while trying to decode final_payload. err : " + err.Error())
+
+			return
+		}
+		if strings.Contains(final_payload.URL, NEW_VC_API_PATH) {
+			fields := strings.FieldsFunc(final_payload.URL, func(r rune) bool {
+				if r == '/' || r == ':' {
+					return true
+				}
+				return false
+			})
+			port, err := strconv.Atoi(fields[2])
+			if err != nil {
+				
+			}
+			for i := 0; i < len(circuits); i++ {
+				if circuits[i].id == request.VC_id {
+					circuits[i].next_node = node{port: port}
+					break
+				}
+			}
+		}
+		if final_payload.Method == GET_METHOD {
+			res, err := http.Get(final_payload.URL)
+			if err != nil {
+
+			}
+			defer res.Body.Close()
+			body, err := ioutil.ReadAll(res.Body)
+			if err != nil {
+
+			}
+			log.Println(res.StatusCode)
+			w.WriteHeader(res.StatusCode)
+			w.Write(body)
+		}
+		if final_payload.Method == POST_METHOD {
+			res, err := http.Post(final_payload.URL, "application/json", bytes.NewBuffer(final_payload.Body))
+			if err != nil {
+				log.Println(err)
+			}
+			defer res.Body.Close()
+			body, err := ioutil.ReadAll(res.Body)
+			log.Println("response status code of post request : " + res.Status)
+			w.WriteHeader(res.StatusCode)
+			w.Write(body)
+		}
+		return
+	}
+	if request.Payload_type == PAYLOAD_ENCRYPTED_TYPE {
+		var cir_index int
+		for i := 0; i < len(circuits); i++ {
+			if circuits[i].id == request.VC_id {
+				cir_index = i
+				break
+			}
+		}
+		resp, err := http.Post("http://localhost:"+strconv.Itoa(circuits[cir_index].next_node.port)+FORWARD_API_PATH, "application/json", bytes.NewBuffer(request.Payload))
+		if err != nil {
+			
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			
+		}
+		w.WriteHeader(resp.StatusCode)
+		w.Write(body)
+	}
+
+}
+
+type final_payload_struct struct {
+	Method string
+	URL    string
+	Body   []byte
+}
+
+type payload_struct struct {
+	VC_id        string
+	Payload_type string
+	Payload      []byte
+}
+
+type new_vc_struct struct {
+	VC_id string
+	incomming_port int
 }
